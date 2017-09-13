abd39803d3d8cf92cfe0dc72838750d41bb3e477
Pedro <pedrogomesyoo@gmail.com>
2017-09-14 00:53:59 +0200

    Add Travis CI testing

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ sudo apt-get install git python python-yaml python-jinja2 aptitude
 cd ~
 mkdir workspace
 cd workspace
-git clone git@github.com:Lusitaniae/linux-desktop-ansible.git
+git clone git@github.com:Lusitaniae/linux-desktop-ansible.git linux-desktop-ansible
