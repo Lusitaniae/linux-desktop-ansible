e28a7639ae667a66c2f27b14ca3e5b059b0e6f7e
Pedro Gomes <pedrogomesyoo@gmail.com>
2018-12-11 20:18:40 +0100

    skip confirmation prompt and fix git clone command

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-sudo apt-get install git python python-yaml python-jinja2 aptitude
+sudo apt-get install  -y git python python-yaml python-jinja2 aptitude
 cd ~
 mkdir workspace
 cd workspace
-git clone git@github.com:Lusitaniae/linux-desktop-ansible.git linux-desktop-ansible
+git clone https://github.com/Lusitaniae/linux-desktop-ansible.git  linux-desktop-ansible
